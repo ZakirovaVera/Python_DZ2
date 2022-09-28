bd38eca875e8b7454c9b4c9bf7a996156727504c
ZakirovaVera <zakirovavk@gmail.com>
2022-09-28 10:03:46 +0500

    форматирование

--- task4.py
+++ task4.py
@@ -3,27 +3,25 @@
 # в файле file.txt в одной строке одно число.
 import random
 user_numberN = int(input('Введите целое число N: '))
-lst_numbers = []
-sum = 0
+string_numbers = []
+sum_position_txt = 0
 
-while len(lst_numbers) < user_numberN:
-    lst_numbers.append(random.randrange(-user_numberN, user_numberN+1))
+while len(string_numbers) < user_numberN:
+    string_numbers.append(random.randrange(-user_numberN, user_numberN+1))
 
-print(lst_numbers)
+print(string_numbers)
 
 with open("text.txt", "r") as file:
-    nums = file.read().splitlines()
-    print(f'Данные с файла {nums}')
+    file_data_list = file.read().splitlines()
+    print(f'Данные с файла {file_data_list}')
     index = 0
-    while index < len(nums):
-        position = int(nums[index])
-        # print(len(lst_numbers))
-        if position > len(lst_numbers):
-            sum +=0
+    while index < len(file_data_list):
+        position = int(file_data_list[index])
+        if position > len(string_numbers):
+            sum_position_txt += 0
         else:
-            sum += lst_numbers[position]
-            print(f'На {position} позиции -> {lst_numbers[position]}')
-        index+=1
-file.close()    
-print(f'Сумма этих позиций = {sum}')
-    
+            sum_position_txt += string_numbers[position]
+            print(f'На {position} позиции -> {string_numbers[position]}')
+        index += 1
+file.close()
+print(f'Сумма этих позиций = {sum_position_txt}')
