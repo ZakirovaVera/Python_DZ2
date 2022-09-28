e6312e25eeef16ecaf7c7706c6f410094e938061
ZakirovaVera <zakirovavk@gmail.com>
2022-09-28 13:46:25 +0500

    решение 5 задачи на перемешиванию списка

--- task5.py
+++ task5.py
@@ -1,1 +1,15 @@
 # 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+
+numbers = [4, 9, 7, 3, 8, 1, 0, 9, 5]
+print(numbers)
+
+index = 0
+while index <= len(numbers)-1:
+    index_rand = int(random.randint(0, len(numbers)-1))
+    temp = numbers[index]
+    numbers[index] = numbers[index_rand]
+    numbers[index_rand] = temp
+    index+=1
+print(numbers)
